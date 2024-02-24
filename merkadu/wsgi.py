8b315aac8a8eec8a8d946986c10460dc5dbd6d7b
Fedevops <fernandosilvadevops@gmail.com>
2024-02-24 15:24:42 -0300

    Separação de ambientes

--- merkadu/wsgi.py
+++ merkadu/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Substitua 'myproject.settings' pelo caminho correto para seus arquivos de configuração
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'merkadu.settings.prod'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'merkadu.settings_prod'))
 
 application = get_wsgi_application()
